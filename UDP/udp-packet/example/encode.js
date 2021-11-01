1db7b9bcaf302ef2c7025a3d1fd40e278320dfe0
rakhapratama <63789297+zerbk@users.noreply.github.com>
2021-11-01 10:51:57 +0700

    Delete UDP/udp-packet directory

--- UDP/udp-packet/example/encode.js
+++
@@ -1,9 +0,0 @@
-var udp = require('../')
-
-console.log(udp.encode({
-  sourceIp: '10.0.0.1',
-  sourcePort: 58936,
-  destinationIp: '10.0.0.2',
-  destinationPort: 80,
-  data: Buffer.from('whatever')
-}))
